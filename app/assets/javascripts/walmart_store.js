88f7a2658d64f87c60aaa77602a092e4015bc41b
Thiago Ganzarolli <thiago.ganzarolli@gmail.com>
2013-07-19 00:33:57 -0300

    aplicacao gerada com estrutura basica de modelos e gems

---
+++ app/assets/javascripts/walmart_store.js
@@ -0,0 +1,13 @@
+window.WalmartStore = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  WalmartStore.initialize();
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WalmartStore::Application.config.secret_token = '9d4e9c6c8e1ef27a477aa5c94102ac8b33749dccbeca0a0f8f476642862be06aa4943da0d376bae67cb36990969d3615edb5c7dce431623b71335b5115a83946'
