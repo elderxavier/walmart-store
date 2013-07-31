6aded1c3ea22e3927fae9e6cd34e27d079d383e2
Thiago Ganzarolli <thiago.ganzarolli@gmail.com>
2013-07-31 20:30:50 -0300

    incluindo atributos

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category
   #attr_accessible :name, :category, :category_id
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :name, type: String
+  field :name, :type => String
   has_many :products
   belongs_to :category, :dependent => :restrict
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,10 @@ class Product
    include Mongoid::Document
    include Mongoid::Timestamps
    belongs_to :category, :dependent => :delete
-   field :name, type: String
-   field :description, type: String
-   field :price, type: BigDecimal
+   field :name, :type => String
+   field :description, :type => String
+   field :price, :type => BigDecimal
+   field :atributos, :type => Hash
    def category_name
      category.name if category
    end
